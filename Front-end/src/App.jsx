490fa56356d0a17b43f29c103370cd7138a6e311
Vuong <nhatvuongnguyen208@gmail.com>
2024-01-22 13:06:51 +0700

    done prj1

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,10 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import StatusChart from "./components/StatusChart";
 import Status from "./components/Status";
+import statusData from "./data/data.json";
+import axios from "axios";
 
 function App() {
+  const { temperatureData, humidityData, brightnessData } = statusData;
+
+  const [allData, setAllData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "http://api.weatherapi.com/v1/forecast.json?key=6c96119eb87d4325a7d21001242201&q=93501&days=14"
+      );
+      const newData = response.data.forecast.forecastday.map((dataObj) => ({
+        date: dataObj.date,
+        temperatureData: dataObj.day.avgtemp_c,
+        humidityData: dataObj.day.avghumidity,
+        brightnessData: dataObj.day.uv,
+      }));
+      setAllData(newData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+
+    const intervalId = setInterval(fetchData, 10000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(allData);
+  }, [allData]);
+
   const [sensorData, setSensorData] = useState({
     temperature: randomValue(5, 45),
     humidity: randomValue(30, 80),
@@ -23,15 +60,15 @@ function App() {
     return () => clearInterval(intervalId);
   }, []);
 
-  const [lightState,setLightState]= useState("Off");
-  const [fanState,setFanState]= useState("Off");
+  const [lightState, setLightState] = useState("Off");
+  const [fanState, setFanState] = useState("Off");
 
-  function handleLight(){
-    lightState==="Off" ? setLightState("On"):setLightState("Off");
+  function handleLight() {
+    lightState === "Off" ? setLightState("On") : setLightState("Off");
   }
 
-  function handleFan(){
-    fanState === "Off" ? setFanState("On"):setFanState("Off");
+  function handleFan() {
+    fanState === "Off" ? setFanState("On") : setFanState("Off");
   }
 
   function randomValue(start, end) {
@@ -46,10 +83,11 @@ function App() {
         <Status name={"brightness"} data={sensorData.brightness} />
       </div>
       <div className="functionsContainer">
-        <StatusChart />
+        <StatusChart allData={allData}
+        />
         <div className="adjustmentContainer">
-          <Button onClick={handleFan} name={"Fan"} state={fanState}/>
-          <Button onClick={handleLight} name={"Light"} state={lightState}/>
+          <Button onClick={handleFan} name={"Fan"} state={fanState} />
+          <Button onClick={handleLight} name={"Light"} state={lightState} />
         </div>
       </div>
     </div>
